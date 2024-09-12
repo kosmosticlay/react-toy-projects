b2557fbf93d8fd8231e4a881cffa1b156c3dbd40
kosmosticlay <kosmosticlay@gmail.com>
2024-09-12 18:51:53 +0900

    feat: Add Header component

---
+++ tic-tac-toe/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full h-24 bg-amber-600 flex-center">
+      <h1 className="text-5xl font-bold">Tic-Tac-Toe</h1>
+    </header>
+  );
+}
