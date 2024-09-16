da1182e7f9adbada8177d62da2d5aa60bfee24e7
kosmosticlay <kosmosticlay@gmail.com>
2024-09-16 22:51:36 +0900

    refactor: Remove SplashScreen prevention code

--- my-expo-app/app/_layout.tsx
+++ my-expo-app/app/_layout.tsx
@@ -1,9 +1,5 @@
-import * as SplashScreen from "expo-splash-screen";
 import "react-native-reanimated";
 import { View, Text, StyleSheet } from "react-native";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
